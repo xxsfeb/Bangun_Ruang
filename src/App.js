ffa2f04c53185733a9435d693f79f955f9cd853a
xxsfeb <sfebiyaulzz@gmail.com>
2021-06-04 16:47:54 +0700

    tugas1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+  const[sisi, setSisi] = useState("") 
+  const[tinggi, setTinggi] = useState("")
+  const[jari, setJari] = useState("")
+  const[Volume, setVolume] = useState("")
+  
+  let phi
+  if(jari%7 === 0){
+    phi = 22/7
+  }
+  else {
+    phi = 3.14
+  }
+
+  console.log(`sisi : ${sisi}`)
+  console.log(`tinggi : ${tinggi}`)
+  console.log(`jari : ${jari}`)
+  console.log(`phi: ${phi}`)
+
+  function Hitung(){
+    setVolume(1/3 * phi * jari**2 * tinggi)
+  }
+
+  return(
+    <div class = "App">
+    <p class="judul">VOLUME KERUCUT</p>
+
+        <p class ="x">
+        Masukan Jari-jari (r) (cm)
+      </p>
+      <input type = "text" value = {jari} onChange = {e=> setJari(+e.target.value)}></input>
+
+      <p class = "x">
+        Masukan Tinggi (t) (cm)
+      </p>
+      <input type = "text" value = {tinggi} onChange = {e=> setTinggi(+e.target.value)}></input>
+
+      <p>
+        <button onClick = {Hitung}>HITUNG</button>
+      </p>
+
+      <p class = "y">
+        {Volume} cm<sup>3</sup>
+      </p>
+
     </div>
   );
-}
 
+}
 export default App;
